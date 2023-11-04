a508fb5d8864135bbe732b6558aa08c208371b8d
Matthew Peters <f1driver05@gmail.com>
2023-11-04 14:42:24 +0000

    first

---
+++ src/main/java/technology/duck/durhacktemplateinnit/Durhacktemplateinnit.java
@@ -0,0 +1,17 @@
+package technology.duck.durhacktemplateinnit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Durhacktemplateinnit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
